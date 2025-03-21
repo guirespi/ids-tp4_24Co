610696ab7ff46596dcd71d38c47a5c7b5da63d28
Guido Ramírez <guidoramirez1998@hotmail.es>
2025-03-20 19:10:16 -0500

    Refactiry for led_test

--- test/test_led.c
+++ test/test_led.c
@@ -3,9 +3,19 @@
 
 #include "led.h"
 
+static uint16_t led_address = 0xffff;
+
+void setUp(void) {
+    led_init(&led_address);
+}
+
+void tearDown(void) {
+    ;
+}
+
 //! @test All initialized leds are turn off.
 void test_all_leds_initialized_start_off(void) {
-    uint16_t led_address = 0xffff;
+    uint16_t led_address = 0xFFFF;
 
     led_init(&led_address);
     TEST_ASSERT_EQUAL_HEX16(0x0000, led_address);
@@ -13,10 +23,8 @@ void test_all_leds_initialized_start_off(void) {
 
 //! @test Turn on a single led.
 void test_turn_on_single_led(void) {
-    uint16_t led_address = 0xffff;
     uint8_t led_pos = 4;
 
-    led_init(&led_address);
     led_turn_on(led_pos);
 
     TEST_ASSERT_EQUAL_HEX16((1 << led_pos), led_address);
@@ -24,10 +32,8 @@ void test_turn_on_single_led(void) {
 
 //! @test Turn off a single led.
 void test_turn_off_single_led(void) {
-    uint16_t led_address = 0xffff;
     uint8_t led_pos = 4;
 
-    led_init(&led_address);
     led_turn_on(led_pos);
     led_turn_off(led_pos);
 
